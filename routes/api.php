050602485d741a036708112d6e2905afe3db41dc
DowneyL <heng8050lee@foxmail.com>
2018-03-19 08:58:18 +0000

    verificationCode store

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,17 @@ use Illuminate\Http\Request;
 |
 */
 
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+*/
+$api = app('Dingo\Api\Routing\Router');
+
+$api->version('v1', [
+    'namespace' => 'App\Http\Controllers\Api'
+], function($api) {
+    // sms verify
+    $api->post('verificationCodes', 'VerificationCodesController@store')
+        ->name('api.verificationCodes.store');
 });
